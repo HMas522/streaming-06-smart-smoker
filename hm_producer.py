357316d283f99d3bb66504440d3c7c02ac80349c
Hayley Massey <HMassey12@outlook.com>
2024-06-09 16:08:45 -0500

    reworked producer

--- hm_producer.py
+++ hm_producer.py
@@ -14,85 +14,84 @@ import csv
 import pika
 import sys
 import webbrowser
-import time
-import struct
-from datetime import datetime
+import pathlib
 from util_logger import setup_logger
 
 # Call setup_logger to initialize logging
 logger, log_file_name = setup_logger(__file__)
 
-def offer_rabbitmq_admin_site():
+#Calling a function to ask the user if they want to see the Admin Webpage of RabbitMQ.
+def offer_rabbitmq_admin_site(show_offer=True):
     """Offer to open the RabbitMQ Admin website"""
-    ans = input("Would you like to monitor RabbitMQ queues? y or n ")
-    print()
-    if ans.lower() == "y":
-        webbrowser.open_new("http://localhost:15672/#/queues")
+    if show_offer:
+        ans = input("Would you like to monitor RabbitMQ queues? y or n ")
         print()
+        if ans.lower() == "y":
+            webbrowser.open_new("http://localhost:15672/#/queues")
+            print()
 
-def send_message(host: str, queue_name: str, message: bytes):
+def send_temps(host: str, queue_name: str):
     """
-    Creates and sends a binary message to the specified queue.
+    Creates and sends a message to the queue each execution.
+    This process runs and finishes.
 
     Parameters:
         host (str): the host name or IP address of the RabbitMQ server
         queue_name (str): the name of the queue
-        message (bytes): the binary message to be sent to the queue
     """
-
     try:
         # create a blocking connection to the RabbitMQ server
         conn = pika.BlockingConnection(pika.ConnectionParameters(host))
         # use the connection to create a communication channel
         ch = conn.channel()
         # use the channel to declare a durable queue
-        ch.queue_declare(queue=queue_name, durable=True)
-        # use the channel to publish a message to the queue
-        ch.basic_publish(exchange="", routing_key=queue_name, body=message)
-        # print a message to the console for the user
-        print(f" [x] Sent message to {queue_name}")
+        # a durable queue will survive a RabbitMQ server restart
+        # and help ensure messages are processed in order
+        # messages will not be deleted until the consumer acknowledges
+        
+        ch.queue_declare(queue="smokerA", durable=True)
+        ch.queue_declare(queue="jackfruit", durable=True)
+        ch.queue_declare(queue="pineapple", durable=True)
+
+        with open('smoker-temps.csv', 'r') as file:
+            reader = csv.reader(file, delimiter=",")
+            header = next(reader) 
+            for row in reader:
+                Time,Channel1,Channel2,Channel3 = row
+                
+                if Channel1: 
+                    smoker_temp = ','.join([Time, Channel1])
+                    ch.basic_publish(exchange="", routing_key="smokerA", body=smoker_temp)
+                    logger.info(f" [*] Smoker Temp is {smoker_temp}")
+                
+                if Channel2:
+                    jackfruit = ','.join([Time, Channel2])
+                    ch.basic_publish(exchange="", routing_key="jackfruit", body=jackfruit)
+                    logger.info(f" [*] Jackfruit Temp is {jackfruit}")
+
+                if Channel3:
+                    pineapple = ','.join([Time, Channel3])
+                    ch.basic_publish(exchange="", routing_key="pineapple", body=pineapple)
+                    logger.info(f" [*] Pineapple Temp is {pineapple}")
+
     except pika.exceptions.AMQPConnectionError as e:
-        print(f"Error: Connection to RabbitMQ server failed: {e}")
+        logger.info(f"Error: Connection to RabbitMQ server failed: {e}")
         sys.exit(1)
     finally:
+       
+
         # close the connection to the server
         conn.close()
 
-# Define how the csv will be read by RabbitMQ
-# data_row tells us how the code will read the columns in our csv
-# Column 0 is our time stamp 
-# Column 1 & Channel 1 is our smoker temp
-# Columnn 2 & Channel 2 is food A
-# Column 3 & Channel 3 is food B
+# Standard Python idiom to indicate main program entry point
+# This allows us to import this module and use its functions
+# without executing the code below.
+# If this is the program being run, then execute the code below
+if __name__ == "__main__":  
+    # ask the user if they'd like to open the RabbitMQ Admin site
+    offer_rabbitmq_admin_site() 
 
-def process_csv():
-    """Process the CSV file and send messages to RabbitMQ queues"""
-    with open("smoker-temps.csv", newline='') as csvfile:
-        reader = csv.reader(csvfile)  
-        next(reader)
-        for data_row in reader:
-                time_stamp_str = data_row[0]
-                smokerA_temp_str = data_row[1]
-                jackfruit_temp_str = data_row[2]
-                pineapple_temp_str = data_row[3]
-
-             # Convert timestamp string to Unix timestamp (float)
-        timestamp = datetime.strptime(time_stamp_str, "%m/%d/%y %H:%M:%S").timestamp()
-
-        if smokerA_temp_str:
-                message = struct.pack('!df', timestamp, float(smokerA_temp_str))
-                send_message("localhost", "smokerA", message)
-            
-        if jackfruit_temp_str:
-                message = struct.pack('!df', timestamp, float(jackfruit_temp_str))
-                send_message("localhost", "jackfruit", message)
-            
-        if pineapple_temp_str:
-                message = struct.pack('!df', timestamp, float(pineapple_temp_str))
-                send_message("localhost", "pineapple", message)
-
-        time.sleep(5)
-
-if __name__ == "__main__":
-    offer_rabbitmq_admin_site()
-    process_csv()
+    # send the message to the queue
+    send_temps("localhost","smokerA")
+    send_temps("localhost","jackfruit")
+    send_temps("localhost","pineapple")
