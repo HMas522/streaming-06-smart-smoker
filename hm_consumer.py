44a60371f60dd612571920705f3aea59519155a9
Hayley Massey <HMassey12@outlook.com>
2024-06-09 15:34:50 -0500

    rewrite of consumer code

--- hm_consumer.py
+++ hm_consumer.py
@@ -16,42 +16,101 @@ Remember:
 # Basic imports to run code
 import pika
 import sys
-import os
 import time
+import struct
+from datetime import datetime
 from collections import deque
-import re
 
 # Define the deques and window
 smokerA_deque = deque(maxlen=5)
 jackfruit_deque = deque(maxlen=20)
 pineapple_deque = deque(maxlen=20)
 
+# Define smokerA call back
+# Use deque/window to append temperature to the limit set
+def smokerA_callback(ch, method, properties, body):
+    timestamp, temperature = struct.unpack('!df', body)
+    timestamp_str = datetime.fromtimestamp(timestamp).strftime("%m/%d/%y %H:%M:%S")
+    print(f" [x] Received from smoker queue: {timestamp_str} - Temperature: {temperature}F")
 
-# define a main function to run the program
-def main(host: str):
-    """ Continuously listen for task messages on a named queue."""
-    queues = ('smokerA-queue', 'jackfruit-queue', 'pineapple-queue')
+    smokerA_deque.append(temperature)
+
+    if len(smokerA_deque) == smokerA_deque.maxlen:
+        if smokerA_deque[0] - temperature >= 15:
+            print(" [!] Smoker temp decreases by 15 F or more in 2.5 min (or 5 readings)  --> smoker alert!")
+
+    print(" [x] Done.")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+# Define first food callback
+def jackfruit_callback(ch, method, properties, body):
+    timestamp, temperature = struct.unpack('!df', body)
+    timestamp_str = datetime.fromtimestamp(timestamp).strftime("%m/%d/%y %H:%M:%S")
+    print(f" [*] Received from food A queue: {timestamp_str} - Temperature: {temperature}F")
+
+    jackfruit_deque.append(temperature)
+
+    if len(jackfruit_deque) == jackfruit_deque.maxlen:
+        if max(jackfruit_deque) - min(jackfruit_deque) <= 1:
+            print(" [!] Food temp change in temp is 1 F or less in 10 min (or 20 readings)  --> food stall alert!")
+
+    print(" [x] Done.")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+# Define second food callback
+def pineapple_callback(ch, method, properties, body):
+    timestamp, temperature = struct.unpack('!df', body)
+    timestamp_str = datetime.fromtimestamp(timestamp).strftime("%m/%d/%y %H:%M:%S")
+    print(f" [*] Received from food B queue: {timestamp_str} - Temperature: {temperature}F")
+
+    pineapple_deque.append(temperature)
+
+    if len(pineapple_deque) == pineapple_deque.maxlen:
+        if max(pineapple_deque) - min(pineapple_deque) <= 1:
+            print(" [!] Food temp change in temp is 1 F or less in 10 min (or 20 readings)  --> food stall alert!")
+
+    print(" [x] Done.")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+def main():
+    """Continuously listen for task messages on named queues."""
+    hn = "localhost"
+
     try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host)
-        )
+        # Create a blocking connection to the RabbitMQ server
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hn))
     except Exception as e:
         print()
         print("ERROR: connection to RabbitMQ server failed.")
-        print(f"Verify the server is running on host={host}.")
+        print(f"Verify the server is running on host={hn}.")
         print(f"The error says: {e}")
         print()
         sys.exit(1)
+
     try:
+        # Use the connection to create a communication channel
         channel = connection.channel()
+
+        # Declare each queue
+        queues = ["smokerA", "jackfruit", "pineapple"]
         for queue in queues:
             channel.queue_delete(queue=queue)
-            channel.queue_declare(queue, durable=True)
+            channel.queue_declare(queue=queue, durable=True)
+
+        # Set the prefetch count to limit the number of messages being processed concurrently
         channel.basic_qos(prefetch_count=1)
-        channel.basic_consume("smokerA-queue", on_message_callback=smokerA_callback, auto_ack=False)
-        channel.basic_consume("jackfruit-queue", on_message_callback=jackfruit_callback, auto_ack=False)
-        channel.basic_consume("pineapple-queue", on_message_callback=pineapple_callback, auto_ack=False)
+
+        # Configure the channel to listen on each queue with corresponding callback function
+        channel.basic_consume(queue="smokerA", on_message_callback=smokerA_callback, auto_ack=False)
+        channel.basic_consume(queue="jackfruit", on_message_callback=jackfruit_callback, auto_ack=False)
+        channel.basic_consume(queue="pineapple", on_message_callback=pineapple_callback, auto_ack=False)
+
+        # Print a message to the console for the user
         print(" [*] Waiting for messages. To exit press CTRL+C")
+
+        # Start consuming messages via the communication channel
         channel.start_consuming()
+
     except Exception as e:
         print()
         print("ERROR: something went wrong.")
@@ -65,83 +124,5 @@ def main(host: str):
         print("\nClosing connection. Goodbye.\n")
         connection.close()
 
-# Help with readability by adding degree sign
-degree_sign = u'\N{DEGREE SIGN}'
-
-# Define a callback function to be called when a message is received
-# time sleep using "."
-# Basic ack to delete from the queue once acknowledged
-def smokerA_callback(ch, method, properties, body):
-    """ Define behavior on getting a message."""
-    print(f" [x] Received {body.decode()}")
-    time.sleep(body.count(b"."))
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    # If not already, turn the temperatures to a float
-    body_decode = body.decode('utf-8')
-    temps = re.findall(r'SmokerA is (\d+\.\d+)', body_decode)
-    temps_float = float(temps[0])
-    smokerA_deque.append(temps_float)
-
-    # If smoker temp decreases by 15 F or more in 2.5 min (or 5 readings)  --> smoker alert!
-    if len(smokerA_deque) == smokerA_deque.maxlen:
-        if smokerA_deque[0] - temps_float > 15:
-            smoker_change = smokerA_deque[0] - temps_float
-            print(f'''
-**************************ALERT****************************
-SmokerA temperature has decreased by {smoker_change}{degree_sign} in 2.5 minutes!
-
-            ''')
-
-# Define first food callback
-def jackfruit_callback(ch, method, properties, body):
-    """ Define behavior on getting a message."""
-    print(f" [x] Received {body.decode()}")    
-    time.sleep(body.count(b"."))
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    body_decode = body.decode('utf-8')
-    temps = re.findall(r'Jackfruit temp is (\d+\.\d+)', body_decode)
-    temps_float = float(temps[0])
-    jackfruit_deque.append(temps_float)
-
-    # If food temp change in temp is 1 F or less in 10 min (or 20 readings)  --> food stall alert!
-    if len(jackfruit_deque) == jackfruit_deque.maxlen:
-        if max(jackfruit_deque) - min(jackfruit_deque) < 1:
-            jackfruit_change = max(jackfruit_deque) - min(jackfruit_deque)
-            print(f'''
-**************************ALERT****************************
-Jackfruit temperature has decreased by {jackfruit_change}{degree_sign} in 10 minutes!
-
-            ''')
-
-# Define second food callback
-def pineapple_callback(ch, method, properties, body):
-    """ Define behavior on getting a message."""
-    print(f" [x] Received {body.decode()}")
-    time.sleep(body.count(b"."))
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    body_decode = body.decode('utf-8')
-    temps = re.findall(r'Pineapple temp is (\d+\.\d+)', body_decode)
-    temps_float = float(temps[0])
-    pineapple_deque.append(temps_float)
-
-    # If food temp change in temp is 1 F or less in 10 min (or 20 readings)  --> food stall alert!
-    if len(pineapple_deque) == pineapple_deque.maxlen:
-        if max(pineapple_deque) - min(pineapple_deque) < 1:
-            pineapple_change = max(pineapple_deque) - min(pineapple_deque)
-            print(f'''
-**************************ALERT****************************
-Pineapple temperature has decreased by {pineapple_change}{degree_sign} in 10 minutes!
-
-            ''')
-
-# ---------------------------------------------------------------------------
-# If this is the script we are running, then call some functions and execute code!
-# ---------------------------------------------------------------------------
 if __name__ == "__main__":
-  # call the main function with the information needed
-
-    host = 'localhost'
-    main(host)
+    main()
